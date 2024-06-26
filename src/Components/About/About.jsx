76d7153a8238b7f217aa02e427dd28e4579718e6
Dani Ren <danirenn16@gmail.com>
2024-06-26 14:47:59 -0700

    Creae about page layout

---
+++ src/Components/About/About.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './About.css'
+import self_img from '../../assets/website-photo.jpg'
+import self_img2 from '../../assets/website-photo2.jpg'
+
+const About = () => {
+  return (
+    <div className='about'>
+      <div className='about-left'>
+        <img src={self_img} alt="" className='about-img'/>
+        {/* <img src={self_img2} alt="" className='about-img'/> */}
+      </div>
+      <div className='about-right'>
+        <h3> ABOUT ME </h3>
+        <h2>Third year university student</h2>
+        <p> I have taken .... courses, done co=op etc etc etc will fill in</p>
+      </div>
+    </div>
+  )
+}
+
+export default About
--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import './Hero.css'
+import arrow from '../../assets/dark-arrow.png'
 
 const Hero = () => {
   return (
-    <div className='hero'>
-
+    <div className='hero container'>
+      <div className='hero-title'>
+        <h1>Welcome to my personal website</h1>
+        <p>I am a third year UBC computer science student </p>
+        <button className='btn'>Learn more <img src={arrow} alt="" /></button>
+      </div>
     </div>
   )
 }
