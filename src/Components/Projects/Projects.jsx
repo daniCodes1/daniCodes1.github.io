3fe186a824c63cab46ccba59639755851337bade
Dani Ren <danirenn16@gmail.com>
2024-06-28 20:51:54 -0700

    Update all the project descriptions

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,47 +1,15 @@
 import React from 'react'
 import './Projects.css'
 
-// const Projects = () => {
-//   return (
-//     <div className='projects'>
-//       <div className="project-card">
-//         <h2>Personal Website</h2>
-//         <h3>Get to know me better in this customized online space!</h3>
-//         <p>JavaScript, React, HTML, CSS</p>
-//         <a href="https://github.com/daniCodes1/Personal-Website">
-//           <button className='btn'>View Project Code</button>
-//         </a>
-//       </div>
-
-//       <div className="project-card">
-//         <h2>Personal Website</h2>
-//         <h3>Get to know me better in this customized online space!</h3>
-//         <p>JavaScript, React, HTML, CSS</p>
-//         <a href="https://github.com/daniCodes1/Personal-Website">
-//           <button className='btn'>View Project Code</button>
-//         </a>
-//       </div>
-
-//       <div className="project-card">
-//         <h2>Personal Website</h2>
-//         <h3>Get to know me better in this customized online space!</h3>
-//         <p>JavaScript, React, HTML, CSS</p>
-//         <a href="https://github.com/daniCodes1/Personal-Website">
-//           <button className='btn'>View Project Code</button>
-//         </a>
-//       </div>
-//     </div>
-//   )
-// }
-
 const Projects = () => {
   const projects = [
     { title: 'Personal Website', description: 'Get to know me better in this customized online space!', tech: 'JavaScript, React, HTML, CSS', link: 'https://github.com/daniCodes1/Personal-Website' },
-    { title: 'Project 2', description: 'Description for project 2', tech: 'Tech stack for project 2', link: 'https://example.com/project2' },
-    { title: 'Project 3', description: 'Description for project 3', tech: 'Tech stack for project 3', link: 'https://example.com/project3' },
-    { title: 'Project 4', description: 'Description for project 4', tech: 'Tech stack for project 4', link: 'https://example.com/project4' },
-    { title: 'Project 5', description: 'Description for project 5', tech: 'Tech stack for project 5', link: 'https://example.com/project5' },
-    { title: 'Project 6', description: 'Description for project 6', tech: 'Tech stack for project 6', link: 'https://example.com/project6' },
+    { title: 'Job Portal', description: 'A relational database Job Portal application', tech: 'SQL, PHP, CSS', link: 'https://github.com/daniCodes1/Job-Portal-Application' },
+    { title: 'IntelligentDashboard', description: 'Manage your daily tasks using real-time statistics and productivity recommendations.', tech: 'JavaScript, CSS, HTML', link: 'https://github.com/daniCodes1/IntelligentDashboard/tree/templateBranch' },
+    { title: 'Movie Manager', description: 'Get personalized movie recommendations, save and revisit favourites', tech: 'Java', link: 'https://github.com/daniCodes1/Movie-Manager' },
+    { title: 'DigitalDiary', description: 'Blend convenience with the timeless art of self-expression. Log in to write, save and view your diary entries.', tech: 'Python', link: 'https://github.com/daniCodes1/DigitalDiary' },
+    { title: 'MoodShift', description: 'An art recommendations app inspired by art therapy forms, created during HackCamp', tech: 'Java', link: 'https://example.com/project4' },
+
   ];
 
   return (
