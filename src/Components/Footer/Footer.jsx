7757f841546f7786e9e4488eda3df3a3f9526bef
Dani Ren <danirenn16@gmail.com>
2024-07-24 15:25:43 -0700

    Create header

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Footer.css'
+const Footer = () => {
+  return (
+    <div className='footer'>
+      <p>Dani Ren</p>
+      <ul>
+        <li>Contact form</li>
+        <li>Email</li>
+        <li>Github</li>
+        <li>LinkedIn</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Footer
