de466da24a64def8f11bf94608438ba6746e0197
Dani Ren <danirenn16@gmail.com>
2024-07-31 10:02:54 -0700

    Make footer interactive

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import './Footer.css'
+import {Link} from 'react-scroll';
+
 const Footer = () => {
   return (
     <div className='footer'>
       <p>Dani Ren</p>
       <ul>
-        <li>Contact form</li>
-        <li>Email</li>
-        <li>Github</li>
-        <li>LinkedIn</li>
+        <li><Link to='contact' smooth={true} offset={-260} duration={500}>Contact Form</Link></li>
+        <li><a href='mailto:danirenn16@gmail.com'>Email</a></li>
+        <li><a href='https://github.com/danicodes1'>GitHub</a></li>
+        <li><a href='https://ca.linkedin.com/in/dani-ren'>LinkedIn</a></li>
       </ul>
     </div>
   )
