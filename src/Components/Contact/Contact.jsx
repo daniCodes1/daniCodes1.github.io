c0f13a6d696224d1e50306a28cf6d7b5f5324519
Dani Ren <danirenn16@gmail.com>
2024-07-30 19:22:52 -0700

    Add contact form

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -5,6 +5,32 @@ import arrow from '../../assets/white-arrow.png'
 import '@fortawesome/fontawesome-free/css/all.css';
 
 const Contact = () => {
+  const [result, setResult] = React.useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "94790097-b59d-4ba2-a874-165442b60297");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      setResult("Form Submitted Successfully");
+      event.target.reset();
+    } else {
+      console.log("Error", data);
+      setResult(data.message);
+    }
+  };
+
+
   return (
     <div className='contact'>
       <div className="contact-col">
@@ -30,18 +56,19 @@ const Contact = () => {
         </ul>
       </div>
       <div className="contact-col">
-        <form>
+        <form onSubmit={onSubmit}>
           <label>Your name</label>
           <input type="text" name='name' placeholder='Enter your name' required/>
           <label>Phone Number</label>
           <input type="tel" name='phone' placeholder='Enter your phone number' required/>
           <label>Write your message here</label>
-          <textarea name="message" rows="6" placeholder='Enter your messagte' required/>
+          <textarea name="message" rows="6" placeholder='Enter your message' required/>
           <button type="submit" className='btn dark-btn'>
             Submit
             <img src={arrow} alt="" />
           </button>
         </form>
+        <span>{result}</span>
       </div>
     </div>
   )
