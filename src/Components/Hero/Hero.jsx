3f513c54ab4c486275156d5c1fd8411f74fe82e4
Dani Ren <danirenn16@gmail.com>
2024-07-30 19:31:26 -0700

    Create home page button

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Hero.css'
 import arrow from '../../assets/dark-arrow.png'
+import {Link} from 'react-scroll';
 
 const Hero = () => {
   return (
@@ -8,7 +9,8 @@ const Hero = () => {
       <div className='hero-title'>
         <h1>Welcome to my personal website</h1>
         <p>I am a third year UBC computer science student </p>
-        <button className='btn'>Learn more <img src={arrow} alt="" /></button>
+        <Link to='about' smooth={true} offset={-100} duration={500}
+          className='btn'>Learn more <img src={arrow} alt="" /></Link>
       </div>
     </div>
   )
