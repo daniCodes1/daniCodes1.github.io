9013b8a4b2d6f9d7f2c3ce6e8792544271479164
Dani Ren <danirenn16@gmail.com>
2024-08-14 13:46:34 -0700

    Update link

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -3,7 +3,7 @@ import './Projects.css'
 
 const Projects = () => {
   const projects = [
-    { title: 'Personal Website', description: 'Get to know me better in this customized online space!', tech: 'JavaScript, React, HTML, CSS', link: 'https://github.com/daniCodes1/Personal-Website' },
+    { title: 'Personal Website', description: 'Get to know me better in this customized online space!', tech: 'JavaScript, React, HTML, CSS', link: 'https://github.com/daniCodes1/daniCodes1.github.io' },
     { title: 'Job Portal', description: 'A relational database Job Portal application', tech: 'SQL, PHP, CSS', link: 'https://github.com/daniCodes1/Job-Portal-Application' },
     { title: 'IntelligentDashboard', description: 'Manage your daily tasks using real-time statistics and productivity recommendations.', tech: 'JavaScript, CSS, HTML', link: 'https://github.com/daniCodes1/IntelligentDashboard/tree/templateBranch' },
     { title: 'Movie Manager', description: 'Get personalized movie recommendations, save and revisit favourites', tech: 'Java', link: 'https://github.com/daniCodes1/Movie-Manager' },
