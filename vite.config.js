5893b371fe4456521debccbdc073082dc3d4556e
Dani Ren <danirenn16@gmail.com>
2024-08-13 20:01:15 -0700

    Update dist

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/',
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Ensure the correct output directory
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'], // Ensure Vite can resolve .jsx files
+  },
 })
